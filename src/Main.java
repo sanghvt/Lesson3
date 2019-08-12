1f65a9bab8ee685b79c1e4f24ba1805c1d3acfd9
Sang Nguyen <sang211nt@gmail.com>
2019-08-12 11:17:18 +0700

    Task2

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Task2 S1 = new Task2();
+        S1.printOut1();
+    }
+}
---
+++ src/Task2.java
@@ -0,0 +1,9 @@
+public class Task2 {
+    private float l = 7;
+    private float n = 5;
+    private float h = 6;
+    private float acreage1 = (float)((l+n)/2*h);
+    void printOut1(){
+        System.out.println("S hinh thang: " + acreage1);
+    }
+}
