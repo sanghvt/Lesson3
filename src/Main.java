8779928b96dc2ff24d8596901e3c56dec79e1bb0
Sang Nguyen <sang211nt@gmail.com>
2019-08-12 11:23:54 +0700

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Task2 S1 = new Task2();
         S1.printOut1();
+        Task3 S2 = new Task3();
+        S2.printout2();
+        
     }
 }
---
+++ src/Task3.java
@@ -0,0 +1,8 @@
+public class Task3 {
+    private float r = 3;
+    private float acreage2 = (float) (Math.pow(r,2)*Math.PI);
+    void printout2(){
+        System.out.println("S hinh tron: " + acreage2);
+    }
+
+}
